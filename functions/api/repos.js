6ce85242247785d0304a84a547a76dcc3207757d
Ekwuno <obinnacodes@gmail.com>
2022-04-16 13:23:41 +0100

    update redirect url

--- functions/api/repos.js
+++ functions/api/repos.js
@@ -31,7 +31,8 @@ export async function onRequestGet({ request, env }) {
 const config = {
   CLIENT_ID: "d00bdb1af84c4faef82c",
   CLIENT_SECRET: "1875475451faf4c8bc1fdc69033fb4da6441d145",
-  REDIRECT_URL: "http://localhost:8788/",
+  REDIRECT_URL:
+    "https://test-functions.cloudflare-functions-oauth-example.pages.dev/",
   AUTHORIZATION_ENDPOINT: "https://github.com/login/oauth/authorize",
   TOKEN_ENDPOINT: "https://github.com/login/oauth/access_token",
   RESOURCE_ENDPOINT: "https://api.github.com/",
